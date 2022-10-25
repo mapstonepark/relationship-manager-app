e55752c1461c1783c3232b6684d1280271fd635d
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-10-25 13:42:41 -0500

    adding login and signup pages

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
+router.get('/login', authController.getLogin)
+router.get('/login', authController.postLogin)
+router.get('/signup', authController.getSignup)
+router.get('/signup', authController.postSignup)
 
 
 module.exports = router
